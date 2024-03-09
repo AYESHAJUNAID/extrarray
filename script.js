fc35452b3ea9d33f9002aa272ebc48ce051872f0
AYESHAJUMAID <abdulhajunaid@gmail.com>
2024-03-09 10:03:51 +0500

    done

---
+++ script.js
@@ -0,0 +1,60 @@
+var slice = [1, 2, 3, 4, 5];
+console.log("original array :", slice);
+var slicemove = slice.slice(1, 4);
+console.log("removed element :", slicemove);
+
+var splice = ["aisha", "laiba", "amna", "areesha", "fiza"];
+console.log("original array :", splice);
+var array = splice.splice(2, 5);
+console.log("removed element :", array);
+
+var shift = [4, 5, 6, 7, 8];
+console.log("original array :", shift);
+var shiftmove = shift.shift();
+console.log("removed element :", shiftmove);
+
+var unshift = [11, 12, 13, 14, 15, 16];
+console.log("original array :", unshift);
+var unshiftmove = unshift.unshift(12, 14);
+console.log("removed element :", unshiftmove);
+
+var push = ["ayesha", "abdullah", "abaan"];
+console.log("original array :", push);
+var pushmove = push.push("adil");
+console.log("element add with push method :", push);
+
+var pop = ["green", "blue", "black", "yellow", "white "];
+console.log("original array :", pop);
+var popmove = pop.pop();
+console.log("element delete with pop method", pop);
+
+var slice = [10, 29, 35, 74, 95];
+console.log("original array :", slice);
+var slicemove = slice.slice(2, 5);
+console.log("move elements with slice :", slicemove);
+
+var splice = [11, 28, 35, 65, 95];
+console.log("original array :", splice);
+var splicemove = splice.slice(3, 4);
+console.log("move elements with splice :", splicemove);
+
+var shift1 = ["kiran", "ayesha", "kainat", "alishba"];
+console.log("original array :", shift1);
+var shiftmove = shift1.shift();
+console.log("removed element :" , shift1);
+
+
+var push1 = [22,23,24,25,26]
+console.log("original array :", push1);
+var pushmove = push1.push(27, 49, 76);
+console.log("element after add with push method :", push1);
+
+
+
+
+
+
+
+
+
+
